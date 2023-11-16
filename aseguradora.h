44985557452880eb557df3f3fc09d9a8494abd8c
RicardoRuiz2005 <150260615+RicardoRuiz2005@users.noreply.github.com>
2023-11-16 08:15:58 -0600

    Update aseguradora.h

--- aseguradora.h
+++ aseguradora.h
@@ -2,45 +2,123 @@
 realizara el pago por ejemplo el enganche, ademas de saber el tipo de seguro que este tendra, esto con el fin de hacer este complejo metodo
 en algo sencillo de usar */
 
-#include<iostream>
-#include<string>
-#include"aseguradora.h"
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
+class Coche{	
+private:
+	string tipoCoche;
+	string marca;
+	float costo;	
+	int velocidad;
 	
-	//Clase coche
-	Coche auto1("Ferrari", 1.4, "Roma", 200);
-	Coche auto2("Porsche", 2, "918", 150);
-	Coche auto3("Mercedes Benz", .5, "Classic", 100);
+public:
+	Coche(string mar, float cost, string tip, int vel) : marca(mar), costo(cost), tipoCoche(tip), velocidad(vel) {}
 	
-	//Clase personal
-	Personal p1("Jacob", 1244, "empleado");
-	Personal p2("Benjamin", 277, "Jefe");
-	Personal p3("Paolo", 6781, "empleado");
+	string getMarca()const{
+		return marca;
+	}
 	
-	//Class Seguro
-	Seguro ase1("Escencial", 200.5, 4);
-	Seguro ase2("Basico", 150, 3);
-	Seguro ase3("Amplio", 100, 1);
+	float getCosto()const{
+		return costo;
+	}
+	string getTipoCoche(){
+		return tipoCoche;
+	}
+	int getVelocidad(){
+		return velocidad;	
+	}
+	void setMarca(string mar){
+		marca = mar;
+	}
+	void setCosto(float cost){
+		costo = cost;
+	}
+	void setTipoCoche(string tip){
+		tipoCoche = tip;
+	}
+	void setVelocidad(int vel){
+		velocidad = vel;
+	}
 	
-	//Metodos coche
-	cout<<"Metodos de coche"<<endl;
-	auto1.datos();
-	auto2.datos();
-	auto3.datos();
+	void datos(){
+		cout<<"El costo del coche "<<tipoCoche<<" de la marca "<<marca<<" es de "<<costo<<" millones de pesos"<<" con velocidad de "<<velocidad<<" Km/h "<<endl;
+	}
+};
+
+class Personal{
+private:
+	string nombre;
+	int id;
+	string rango;
 	
-	//Metodos personal
-	cout<<endl<<"Metodos de personal"<<endl;
-	p1.datosPersonal();
-	p2.datosPersonal();
-	p3.datosPersonal();
+public:
+	Personal(string nom, int _id, string rang) : nombre(nom), id(_id), rango(rang) {}
 	
-	//Metodos seguro
-	cout<<endl<<"Metodos de seguro"<<endl;
-	ase1.datosSeguro();
-	ase2.datosSeguro();
-	ase3.datosSeguro();
+	string getNombre(){
+		return nombre;
+	}
 	
-	return 0;
-}
+	int getId(){
+		return id;
+	}
+	
+	string getRango(){
+		return rango;
+	}
+	
+	void setNombre(string nom){
+		nombre = nom;
+	}
+	
+	void setId(int _id){
+		id = _id;
+	}
+	
+	void setRango(string rang){
+		rango = rang;
+	}
+	
+	void datosPersonal(){
+		cout<<"El nombre del "<<rango<<" es "<<nombre<<" su ID: "<<id<<endl;
+	}
+};
+
+class Seguro{
+	private:
+		string tipo;
+		int tiempo;
+		float precio;
+	
+	public:
+		Seguro(string tip, float pre, int tiem) : tipo(tip), precio(pre), tiempo(tiem){}
+		
+	string getTipo(){
+		return tipo;	
+	}
+	
+	float getPrecio(){
+		return precio;
+	}
+	
+	int getTiempo(){
+		return tiempo;
+	}
+	
+	void setTipo(string tip){
+		tipo = tip;
+	}
+	
+	void setPrecio(float pre){
+		precio = pre;
+	}
+	
+	void setTiempo(int tiem){
+		tiempo = tiem;
+	}
+	
+	void datosSeguro(){
+		cout<<"El tipo de seguro es: "<<tipo<<", este seguro dura "<<tiempo<<" anios con un costo de '"<<precio<<"'"<<endl;
+	}
+};
