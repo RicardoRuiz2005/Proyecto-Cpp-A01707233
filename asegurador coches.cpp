01a8c29b11d797ebe4b44d688e6d72804a48c665
RicardoRuiz2005 <150260615+RicardoRuiz2005@users.noreply.github.com>
2023-11-23 01:33:45 -0600

    Update asegurador coches.cpp

--- asegurador coches.cpp
+++ asegurador coches.cpp
@@ -4,15 +4,18 @@ en algo sencillo de usar */
 
 #include<iostream>
 #include<string>
-#include"aseguradora.h"
+#include"Personal.h"
+#include"Coche.h"
+#include"Seguro.h"
+
 using namespace std;
 
 int main(){
 	
 	//Clase coche
-	Coche auto1("Ferrari", 1.4, "Roma", 200);
-	Coche auto2("Porsche", 2, "918", 150);
-	Coche auto3("Mercedes Benz", .5, "Classic", 100);
+	Alta *auto1 = new Alta("Ferrari", 1.4, "Roma", 200, 60, false, "un anio de seguro gratis");
+	Media *auto2 = new Media("Porsche", 2, "918", 150, 90, true);
+	Baja *auto3 = new Baja("Mercedes Benz", .5, "Classic", 100, "un par de llantas gratis");
 	
 	//Clase personal
 	Personal p1("Jacob", 1244, "empleado");
@@ -25,19 +28,22 @@ int main(){
 	Seguro ase3("Amplio", 100, 1);
 	
 	//Metodos coche
-	cout<<"Metodos de coche"<<endl;
-	auto1.datos();
-	auto2.datos();
-	auto3.datos();
+	cout<<"-----Metodos de coche----- \n"<<endl;
+	auto1->datos();
+	auto1->probar();
+	
+	auto2->datos();
+	
+	auto3->datos();
 	
 	//Metodos personal
-	cout<<endl<<"Metodos de personal"<<endl;
+	cout<<endl<<"-----Metodos de personal----- \n"<<endl;
 	p1.datosPersonal();
 	p2.datosPersonal();
 	p3.datosPersonal();
 	
 	//Metodos seguro
-	cout<<endl<<"Metodos de seguro"<<endl;
+	cout<<endl<<"-----Metodos de seguro----- \n"<<endl;
 	ase1.datosSeguro();
 	ase2.datosSeguro();
 	ase3.datosSeguro();
