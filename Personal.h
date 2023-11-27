cccbbfe29ee66d56118b3599e86491d1f1380abd
RicardoRuiz2005 <150260615+RicardoRuiz2005@users.noreply.github.com>
2023-11-26 19:12:12 -0600

    Update Personal.h

--- Personal.h
+++ Personal.h
@@ -2,8 +2,12 @@
 realizara el pago por ejemplo el enganche, ademas de saber el tipo de seguro que este tendra, esto con el fin de hacer este complejo metodo
 en algo sencillo de usar */
 
+#ifndef Personales_H_
+#define Personales_H_
+
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 
@@ -15,7 +19,9 @@ private:
 	string rango;
 	
 public:
+	Personal(){};
 	Personal(string nom, int _id, string rang) : nombre(nom), id(_id), rango(rang) {}
+	string to_string();
 	
 	string getNombre(){
 		return nombre;
@@ -45,3 +51,12 @@ public:
 		cout<<"El nombre del "<<rango<<" es "<<nombre<<" su ID: "<<id<<endl;
 	}
 };
+
+string Personal::to_string(){
+	stringstream aux;
+	aux << nombre <<" y tiene el rango de "<<rango<<endl;
+	aux <<"si se desea contactar busque el siguiente ID: "<<id<<endl;
+	return aux.str();	
+}
+
+#endif
